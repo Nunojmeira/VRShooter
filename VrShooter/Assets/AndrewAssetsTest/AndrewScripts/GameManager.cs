336ee3c8b705bedf20fd0ced2ef79e6560ecd900
Andrew Komlyev <andrew.komlyev@gmail.com>
2022-11-27 14:33:18 -0800

    Play Area Added
    Added the shooting range where the player is located. We can easily scale it up or down in any direction if needed it is a proof of concept

---
+++ VrShooter/Assets/AndrewAssetsTest/AndrewScripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //private variables
+    private int _killscore;
+    private int _highscore;
+
+    //serialized variables
+    [SerializeField] private GameObject livesHoloDeck;
+    
+
+    //public variables
+    public static GameManager Instance;//Instance of GameManager
+
+    private void Awake()
+    {
+        if(Instance==null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if(Instance!=this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
