19dfbac4e6c59bcb4484323bc2fde3ac8773183a
WinterPL <95836645+WinterPL@users.noreply.github.com>
2022-11-30 11:48:41 -0800

    bug behavior code(sample)

--- VrShooter/Assets/WINTER PLAY GROUND/Scripts/BUGbehevior.cs
+++ VrShooter/Assets/WINTER PLAY GROUND/Scripts/BUGbehevior.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BUGbehevior : MonoBehaviour
 {
+    [SerializeField] private float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class BUGbehevior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0,0,speed * Time.deltaTime);
     }
 }
