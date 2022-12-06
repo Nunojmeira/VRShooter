350d4784d2532bfecd0b5d1517d7371cbf40779b
WinterPL <95836645+WinterPL@users.noreply.github.com>
2022-12-05 18:24:03 -0800

    Button click implement

--- VrShooter/Assets/WINTER PLAY GROUND/Scripts/ButtonClick.cs
+++ VrShooter/Assets/WINTER PLAY GROUND/Scripts/ButtonClick.cs
@@ -2,28 +2,19 @@
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class ButtonClick : MonoBehaviour, IPointerClickHandler
-{
-    /// <summary>
-    /// Just by implementing IPointerClickHandler like any UI, you can click on objects
-    /// </summary>
-    public UnityEvent OnClick;
-        
-
-    public void OnPointerClick(PointerEventData eventData)
+public class ButtonClick : MonoBehaviour {
+    [SerializeField] private bool isExit = true;
+    private void OnCollisionEnter(Collision collision)
     {
-        OnClick.Invoke();
+        if(collision.gameObject.tag == "Bullet" && isExit)
+        {
+            Debug.Log("Exit");
+        }
+        else if (collision.gameObject.tag == "Bullet" && !isExit)
+        {
+            Debug.Log("RESTART");
+        }
     }
 
-    public void ExitGame()
-    {
-        Debug.Log("EXIT");
-        //Application.Quit();
-    }
-
-    public void RestartGame()
-    {
-        Debug.Log("Restart");
-    }
 
 }
