1d4286c39b15b184e6a57b9e07a3a017303fd66b
WinterPL <95836645+WinterPL@users.noreply.github.com>
2022-12-05 18:10:08 -0800

    exit and restart UI made

---
+++ VrShooter/Assets/WINTER PLAY GROUND/Scripts/ButtonClick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ButtonClick : MonoBehaviour, IPointerClickHandler
+{
+    /// <summary>
+    /// Just by implementing IPointerClickHandler like any UI, you can click on objects
+    /// </summary>
+    public UnityEvent OnClick;
+        
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnClick.Invoke();
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("EXIT");
+        //Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        Debug.Log("Restart");
+    }
+
+}
